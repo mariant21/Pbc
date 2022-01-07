d54045a00ad818366dd5b7b816e683d0a5220bfa
mariant21 <marian_t_95@yahoo.ro>
2022-01-08 00:51:56 +0200

    secont

---
+++ Interval2/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int a, b, x;
+	cin >> a >> b >> x;
+	if (x >= a && x <= b)
+	{
+		cout << "DA";
+	else
+		cout << "NU";
+	}
+
+	return 0;
+}
