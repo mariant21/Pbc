0d362cd08245042fe785cb49515c5aa2f400e837
mariant21 <marian_t_95@yahoo.ro>
2022-01-10 01:16:02 +0200

    ez

---
+++ AfisareNumere/Source.cpp
@@ -0,0 +1,13 @@
+//https://www.pbinfo.ro/probleme/327/afisarenumere
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << i << " ";
+	}
+
+	return 0;
+}
